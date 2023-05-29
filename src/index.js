6498f105d4f6ead2e433049b03ca578f17c67d45
Oscar Bustos <oscarbustosgarrido@gmail.com>
2023-05-29 10:33:04 +0200

    feat: add better ms

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const { startMartinCidReviewService } = require("./services/MartinCidService");
 const {
   startSeriementeReviewService,
 } = require("./services/SeriementeService");
-const { getRandomDelay } = require("./utils/helper");
+const { getRandomDelay, msToTime } = require("./utils/helper");
 
 const app = express();
 
@@ -26,21 +26,27 @@ const runServicesInSequence = async () => {
   console.log("Finished RogerEbertReviewService.");
 
   const delay1 = getRandomDelay();
-  console.log(`Waiting for ${delay1}ms before starting next service.`);
+  console.log(
+    `Waiting for ${msToTime(delay1)}ms before starting next service.`
+  );
   setTimeout(async () => {
     console.log("Starting MartinCidReviewService...");
     await startMartinCidReviewService();
     console.log("Finished MartinCidReviewService.");
 
     const delay2 = getRandomDelay();
-    console.log(`Waiting for ${delay2}ms before starting next service.`);
+    console.log(
+      `Waiting for ${msToTime(delay2)}ms before starting next service.`
+    );
     setTimeout(async () => {
       console.log("Starting SeriementeReviewService...");
       await startSeriementeReviewService();
       console.log("Finished SeriementeReviewService.");
 
       const delay3 = getRandomDelay();
-      console.log(`Waiting for ${delay3}ms before starting next service.`);
+      console.log(
+        `Waiting for ${msToTime(delay3)}ms before starting next service.`
+      );
       setTimeout(runServicesInSequence, delay3);
     }, delay2);
   }, delay1);
